863e85d998403ed91b31b9103fcacd93bbbb6073
pardeep-png <74077725+pardeep-png@users.noreply.github.com>
2022-02-22 22:09:37 -0800

    added, AND, OR, DIVIDE, NEGATE..

--- intermediate/Node.h
+++ intermediate/Node.h
@@ -16,15 +16,15 @@ namespace intermediate {
     enum class NodeType
     {
         PROGRAM, COMPOUND, ASSIGN, LOOP, TEST, WRITE, WRITELN,
-        ADD, SUBTRACT, MULTIPLY, DIVIDE, EQ,NE, LT,LE,  GT, GE, NOT,
-        VARIABLE, INTEGER_CONSTANT, REAL_CONSTANT, STRING_CONSTANT
+        ADD, SUBTRACT, MULTIPLY, DIVIDE, EQ,NE, LT,LE,  GT, GE, NOT,NEGATE, IF,
+        VARIABLE, INTEGER_CONSTANT, REAL_CONSTANT, STRING_CONSTANT, AND, OR, DIV_INTEGER
     };
 
     static const string NODE_TYPE_STRINGS[] =
             {
-                    "PROGRAM", "COMPOUND", "ASSIGN", "LOOP", "TEST", "WRITE", "WRITELN",
-                    "ADD", "SUBTRACT", "MULTIPLY", "DIVIDE", "EQ", "NE", "LT", "LE", "GT", "GE", "NOT",
-                    "VARIABLE", "INTEGER_CONSTANT", "REAL_CONSTANT", "STRING_CONSTANT"
+                    "PROGRAM", "COMPOUND", "ASSIGN", "LOOP", "TEST",  "WRITE", "WRITELN",
+                    "ADD", "SUBTRACT", "MULTIPLY", "DIVIDE", "EQ", "NE", "LT", "LE", "GT", "GE", "NOT", "NEGATE","IF"
+                    "VARIABLE", "INTEGER_CONSTANT", "REAL_CONSTANT", "STRING_CONSTANT", "AND", "OR", "DIV_INTEGER"
             };
 
     constexpr NodeType PROGRAM          = NodeType::PROGRAM;
@@ -37,19 +37,23 @@ namespace intermediate {
     constexpr NodeType ADD              = NodeType::ADD;
     constexpr NodeType SUBTRACT         = NodeType::SUBTRACT;
     constexpr NodeType MULTIPLY         = NodeType::MULTIPLY;
-    constexpr NodeType DIVIDE           = NodeType::DIVIDE;
+    constexpr NodeType DIVIDE           = NodeType::DIVIDE; //this one is for slash
     constexpr NodeType EQ               = NodeType::EQ;
     constexpr NodeType NE               = NodeType::NE; //added
     constexpr NodeType LT               = NodeType::LT; //added
     constexpr NodeType LE               = NodeType::LE; //added
     constexpr NodeType GT               = NodeType::GT; //added
     constexpr NodeType GE               = NodeType::GE; //added
-    constexpr NodeType NOT              = NodeType::NOT; //added   
+    constexpr NodeType NOT              = NodeType::NOT; //added 
+    constexpr NodeType NEGATE           = NodeType::NEGATE;//added
+    constexpr NodeType IF               = NodeType::IF; //added 
     constexpr NodeType VARIABLE         = NodeType::VARIABLE;
     constexpr NodeType INTEGER_CONSTANT = NodeType::INTEGER_CONSTANT;
     constexpr NodeType REAL_CONSTANT    = NodeType::REAL_CONSTANT;
     constexpr NodeType STRING_CONSTANT  = NodeType::STRING_CONSTANT;
-
+    constexpr NodeType AND              = NodeType::AND; //added 
+    constexpr NodeType OR               = NodeType::AND; //added 
+    constexpr NodeType DIV_INTEGER      = NodeType::DIV_INTEGER; //added
     class Node
     {
     public:
